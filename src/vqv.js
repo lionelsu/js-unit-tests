c0979a38eb639773b68059512346902c4fa47e24
lionelsu <lionel_suicune@hotmail.com>
2023-02-28 16:13:55 -0400

    req: done with the vqv function, write a string with name and age

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,20 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  // if para  inverter a verificação dos parâmetros que não forem recebidos, o problema aqui é que sem a atribuição !name... o valor seria declarado como falso e o bloco if não seria executado, por isso invertemos ele para que seja verdadeiro caso não tenha parâmetros
+  if (!name || !age) {
+    return;
+  }
+
+  // Separando as variáveis em duas partes pq o lint não deixa executar com mais de 120 caracteres 
+  // Uso dos \n para quebrar linha com precisão.
+  const firstPart = `Oi, meu nome é ${name}!\nTenho ${age} anos,`;
+  const lastPart = 'trabalho na Trybe e mando muito em programação!\n#VQV!';
+  const message = `${firstPart}\n${lastPart}`;
+
+  // retorno da string construída
+  return message;
+};
 
 module.exports = vqv;
