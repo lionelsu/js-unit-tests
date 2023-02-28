f72fd1721e4fcbf3c26c80c17d69849e0d129dd4
lionelsu <lionel_suicune@hotmail.com>
2023-02-28 19:10:14 -0400

    req: done with objectPlaygound function

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+// função extremamente simples que retorna implicitamente um objecto com uma chave para cada operação com a operação em si de soma, multiplicação, divisão com o valor arredondado para baixo e subtração
+const calculator = (number1, number2) => ({
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+});
 
-const arrayGenerator = (type, object) => {};
+// Esta função serve para gerar chaves, valores ou entradas de acordo com o tipo passado como parametro
+const arrayGenerator = (type, object) => {
+    if (type === 'keys') {
+        return Object.keys(object);
+    }
 
+    if (type === 'values') {
+        return Object.values(object);
+    }
+
+    if (type === 'entries') {
+        return Object.entries(object);
+    }
+};
 module.exports = { calculator, arrayGenerator };
